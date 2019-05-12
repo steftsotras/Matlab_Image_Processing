7007faf8e88041f254ea5872ada6b7cc937b07e3
Stefanos Tsotras <steftsotras2@gmail.com>
2019-05-12 19:02:24 +0300

    initial

---
+++ Exercise_1/functions/linear_image_trans.m
@@ -0,0 +1,15 @@
+function trans_im = linear_image_trans(im)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+
+
+trans_im = zeros(size(im,1),size(im,2));
+
+for i = 1:size(im,1)
+    for j = 1:size(im,2)
+        trans_im(i,j) = im{u,v}(1,:);
+    end
+end
+
+end
+
---
+++ Exercise_1/functions/linear_transformation.m
@@ -0,0 +1,6 @@
+function z2 = linear_transformation(z,a,b,c,d)
+%UNTITLED Summary of this function goes here
+%   Detailed explanation goes here
+z2 = ((d-c)/(b-a))*(z-a) + c;
+end
+
