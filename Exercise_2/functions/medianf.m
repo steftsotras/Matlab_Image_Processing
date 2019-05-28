cce7473d82e70e2390df12e7300b1a198471ca8b
Stefanos Tsotras <steftsotras2@gmail.com>
2019-05-28 18:00:07 +0300

    2.3

---
+++ Exercise_2/functions/medianf.m
@@ -0,0 +1,29 @@
+function medianf(Im)
+
+%getting size of image and defining the output matrix
+[m,n] = size(Im);
+med_Im = zeros(m,n);
+
+%iterate through the image matrix
+for i=1:m
+    for j=1:n
+        
+        %neighborhood boundaries
+        rmin = max(1,i-1);
+        rmax = min(m,i+1);
+        cmin = max(1,j-1);
+        cmax = min(n,j+1);
+        
+        %neighborhood matrix
+        temp = Im(rmin:rmax,cmin:cmax);
+        
+        %average of neighborhood will be the new value of Im(i,j)
+        med_Im(i,j) = median(temp(:));
+        
+    end
+end
+
+
+imshow(med_Im,[0 255]);
+
+end
